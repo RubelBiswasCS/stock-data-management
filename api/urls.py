420cfb813952ffd7c8f540d1903f43fb605f2adf
RubelBiswasCS <rubelbiswas36@gmail.com>
2022-03-28 00:41:41 +0600

    added api views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import StockList
+from .views import StockList, StockDetail
 app_name = 'api'
 
 urlpatterns = [
-    path('stock/',StockList.as_view(),name="stock"),
-    
+    path('stock/',StockList.as_view(),name="stocklist"),
+    path('stock/<int:pk>/', StockDetail.as_view(),name='stockdetail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,22 @@
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from .serializers import StockSerializer
 from smdata_analyzer.models import Stock
 
-class StockList(APIView):
-    def get(self,request):
-        stocks = Stock.objects.all()
-        stocks = StockSerializer(stocks,many=True)
-    
-        data = stocks.data
-        return Response(data,status=status.HTTP_200_OK)
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class StockList(generics.ListCreateAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    pagination_class = StandardResultsSetPagination
+
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
